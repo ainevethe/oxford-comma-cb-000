8211e71a89bd92059a26b3c8a9207eae7217e19b
Crista Cooper <aine3d@gmail.com>
2017-11-16 22:33:12 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,6 @@
 def oxford_comma(array)
-
-if array.length == 2
-  return array.join(" and")
-elsif 
-
-
-  array.join(", ")
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+  array.join(" and ")
 end
